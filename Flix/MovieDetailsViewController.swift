6070524b5c7a5d680f023b6498ba4b5aa09c5ef4
tatiana <khen.tatiana@gmail.com>
2020-09-26 01:06:23 -0700

    Started Part 2

---
+++ Flix/MovieDetailsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MovieDetailsViewController.swift
+//  Flix
+//
+//  Created by Tatiana on 9/26/20.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+    var movie : [String : Any]!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var backdrop: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!
+        
+       posterView.af.setImage(withURL: posterUrl)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -34,8 +34,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
             self.movies = dataDictionary["results"] as! [[String : Any]]
             self.tableView.reloadData()
-            print(dataDictionary)
-
+            
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
               // TODO: Reload your table view data
@@ -68,13 +67,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
 
     /*
-    // MARK: - Navigation
+     MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     In a storyboard-based application, you will often want to do a little preparation before navigation    */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+      //   Pass the selected object to the new view controller.
+        print("Loading up the details screen")
+        
+        //Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        //Deselect movie after tapping
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
-    */
+ 
 
 }
